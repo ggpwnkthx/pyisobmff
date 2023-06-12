d3babaeab7f6db790931fc41cfa0cb6a0813f093
Isaac Jessup <ibjessup@gmail.com>
2023-06-12 18:27:41 -0400

    formatting

--- isobmff/registries/types.py
+++ isobmff/registries/types.py
@@ -1,18 +1,18 @@
 # File: libs/utils/isobmff/registries/types.py
 
 from . import Registry
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import typing
 
 DECODERS = Registry(types=[typing.Callable])
 DECODERS["default"] = lambda _, data: data
 DECODERS["int"] = lambda _, data: int.from_bytes(data, byteorder="big")
-DECODERS["datetime"] = lambda _, data: datetime.utcfromtimestamp(
-    int.from_bytes(data, byteorder="big")
-)
+DECODERS["datetime"] = lambda _, data: datetime(
+    1904, 1, 1, 0, 0, 0, 0, timezone.utc
+) + timedelta(seconds=int.from_bytes(data, byteorder="big"))
 DECODERS["string"] = lambda _, data, encoding="utf-8": data.decode(encoding)
 DECODERS["lang"] = lambda _, data: (
-    chr(((int.from_bytes(data, 'big') >> 10) & 0x1F) + 0x60) +
-    chr(((int.from_bytes(data, 'big') >> 5) & 0x1F) + 0x60) +
-    chr((int.from_bytes(data, 'big') & 0x1F) + 0x60)
+    chr(((int.from_bytes(data, "big") >> 10) & 0x1F) + 0x60)
+    + chr(((int.from_bytes(data, "big") >> 5) & 0x1F) + 0x60)
+    + chr((int.from_bytes(data, "big") & 0x1F) + 0x60)
 )
