f5e1d345f29e84ef0cc649052219ba7d35a49edf
Isaac B. Jessup <ibjessup@gmail.com>
2023-06-25 16:51:19 -0400

    stri, stsg

--- isobmff/box/track/sub.py
+++ isobmff/box/track/sub.py
@@ -13,8 +13,31 @@ class SubTrack(Box):
 
 
 class SubTrackInformation(FullBox, ChildlessBox):
-    # Not implemented
-    pass
+    @functools.cached_property
+    def switch_group(self) -> int:
+        start = super().header_size
+        return self.slice.subslice(start, start + 2).unpack(">H")[0]
+
+    @functools.cached_property
+    def alternate_group(self) -> int:
+        start = super().header_size + 2
+        return self.slice.subslice(start, start + 2).unpack(">H")[0]
+
+    @functools.cached_property
+    def sub_track_ID(self) -> int:
+        start = super().header_size + 4
+        return self.slice.subslice(start, start + 4).unpack(">I")[0]
+
+    @functools.cached_property
+    def attribute_list(self) -> typing.Tuple[str, ...]:
+        start = super().header_size + 8
+        return tuple(
+            e[0] for e in self.slice.subslice(start, self.end).iter_unpack(">4s")
+        )
+
+    @property
+    def header_size(self) -> int:
+        return self.size
 
 
 class SubTrackDefinition(Box):
@@ -22,15 +45,36 @@ class SubTrackDefinition(Box):
 
 
 class SubTrackSampleGroupBox(FullBox):
-    # Not implemented
-    pass
+    @functools.cached_property
+    def grouping_type(self) -> int:
+        start = super().header_size
+        return self.slice.subslice(start, start + 4).unpack(">I")[0]
+
+    @functools.cached_property
+    def item_count(self) -> int:
+        start = super().header_size + 4
+        return self.slice.subslice(start, start + 2).unpack(">H")[0]
+
+    @functools.cached_property
+    def items(self) -> typing.Tuple[int, ...]:
+        start = super().header_size + 8
+        return tuple(
+            e[0]
+            for e in self.slice.subslice(
+                start, start + self.item_count * 4
+            ).iter_unpack(">I")
+        )
+
+    @property
+    def header_size(self) -> int:
+        return super().header_size + 8 + self.item_count * 4
 
 
 BOX_TYPES.update(
     {
         "strk": SubTrack,  # 8.14.3
-        # "stri": SubTrackInformation,  # 8.14.4
+        "stri": SubTrackInformation,  # 8.14.4
         "strd": SubTrackDefinition,  # 8.14.5
-        # "stsg": SubTrackDefinition,  # 8.14.6
+        "stsg": SubTrackDefinition,  # 8.14.6
     }
 )
