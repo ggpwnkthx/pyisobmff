272d10d790c1acbc84fb742fce7ee33e5f8d3d66
Isaac B. Jessup <ibjessup@gmail.com>
2023-06-25 16:59:41 -0400

    stvi

--- isobmff/box/movie/post_decoder.py
+++ isobmff/box/movie/post_decoder.py
@@ -13,13 +13,44 @@ class RestrictedSchemeInfoBox(Box):
 
 
 class StereoVideoBox(FullBox, ChildlessBox):
-    # Not implemented
-    pass
+    @functools.cached_property
+    def reserved(self) -> int:
+        start = super().header_size
+        return int.from_bytes(self.slice.subslice(start, start + 4).read(), byteorder="big") >> 2
+    
+    @functools.cached_property
+    def single_view_allowed(self) -> int:
+        start = super().header_size
+        return int.from_bytes(self.slice.subslice(start, start + 4).read(), byteorder="big") & 0b11
+    
+    @functools.cached_property
+    def stereo_scheme(self) -> int:
+        start = super().header_size + 4
+        return self.slice.subslice(start, start + 4).unpack(">I")[0]
+    
+    @functools.cached_property
+    def length(self) -> int:
+        start = super().header_size + 8
+        return self.slice.subslice(start, start + 4).unpack(">I")[0]
+
+    @functools.cached_property
+    def stereo_indication_type(self) -> typing.Tuple[int, ...]:
+        start = super().header_size + 12
+        return tuple(
+            e[0]
+            for e in self.slice.subslice(
+                start, start + self.length
+            ).iter_unpack(">B")
+        )
+
+    @property
+    def header_size(self) -> int:
+        return super().header_size + 12 + self.length
 
 
 BOX_TYPES.update(
     {
         "rinf": RestrictedSchemeInfoBox,  # 8.15.3
-        # "stvi": StereoVideoBox,  # 8.15.4
+        "stvi": StereoVideoBox,  # 8.15.4
     }
 )
